3a80f543ece026647db0582713111f0ac85bc736
Mohit <mohit@Mohit.Mohit>
2021-05-14 20:08:05 +0530

    initial commit

--- eTicket/urls.py
+++ eTicket/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('', views.index, name = 'index'),
+    path('disputeTicket/', views.disputeTicket, name = 'disputeTicket'),
+    path('generateTicket/', views.generateTicket, name = 'generateTicket'),
+    path('todayOffer/', views.todayOffer, name = 'todayOffer'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ticketApp/views.py
+++ ticketApp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'ticketApp/index.html')
+	
+	
+def disputeTicket(request):
+	return render(request, 'ticketApp/disputeTicket.html')
+
+	
+def generateTicket(request):
+	return render(request, 'ticketApp/generateTicket.html')
+
+	
+def todayOffer(request):
+	return render(request, 'ticketApp/todayOffer.html')
