16ad3139c2938c993fd3f754ecfd7da7348c00f7
Mohit <mohit@Mohit.Mohit>
2021-05-14 18:41:07 +0530

    initial commit

--- ticketApp/static/ticketApp/JS/script.js
+++ ticketApp/static/ticketApp/JS/script.js
@@ -1,1 +1,17 @@
 // We are here to power Website in Run time....
+//alert("hello")
+const slider = $(".slider-item");
+slider.slick({
+  autoplay: true,
+  autoplaySpeed: 1500,
+  dots: true
+});
+slider.on("wheel", function(e) {
+  e.preventDefault();
+
+  if (e.originalEvent.deltaY < 0) {
+    $(this).slick("slickNext");
+  } else {
+    $(this).slick("slickPrev");
+  }
+});
